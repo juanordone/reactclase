3b5be9bc16299560db1fb2e94338f22463c05ce7
juanordone <juan.ordonez89@hotmail.es>
2022-12-14 18:45:34 +0100

    react proyecto ecomerce

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Header from './components/footer/header/Header'
+
+
+
+function App() {
+  
+
+  return(
+    <div className='App'>
+      <Header/>
+
+    </div>
+    
+  )
+}
+
+export default App
