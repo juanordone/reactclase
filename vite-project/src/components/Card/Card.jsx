44497672836f6bb09aadc7707de38479a5b94550
juanordone <juan.ordonez89@hotmail.es>
2023-01-28 10:53:34 +0100

    cambio funcion fetch a la vista

--- vite-project/src/components/Card/Card.jsx
+++ vite-project/src/components/Card/Card.jsx
@@ -1,51 +1,27 @@
 import { Link } from "react-router-dom";
-import { useEffect, useState } from "react";
 
-export default function Card() {
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch("http://localhost:3000/product");
-        const data = await response.json();
-        setProducts(data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    fetchData();
-  }, [setProducts]);
-
+export default function Card(product) {
   return (
     <>
-      <div className="d-flex flex-wrap  ">
-        {products.map((product) => (
-          <div
-            className="card mb-5 mt-5 ms-5 "
-            key={product.id}
-            style={{ width: "18rem" }}
-          >
-            <img
-              src="https://www.maillotmag.com/sites/default/files/orbea_orca_omx_fund_euskadi_2.jpg"
-              className="card-img-top"
-              alt="..."
-            />
-            <div className="card-body">
-              <div>
-                <h5 className="card-title">{product.nombre}</h5>
-                <p className="card-text">
-                  {product.precio} <i className="bi bi-currency-euro"></i>
-                </p>
-                <Link to={"/Producto"} className="btn btn-primary btn-lg">
-                  <i className="bi bi-plus-circle"></i> Información
-                </Link>
-               
-              </div>
-              
+      <div className="d-flex">
+        <div className="card mb-5 mt-5 ms-5 " style={{ width: "18rem" }}>
+          <img
+            src="https://www.maillotmag.com/sites/default/files/orbea_orca_omx_fund_euskadi_2.jpg"
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <div>
+              <h5 className="card-title">{product.nombre}</h5>
+              <p className="card-text">{product.precio}
+                <i className="bi bi-currency-euro"></i>
+              </p>
+              <Link to={"/Producto"} className="btn btn-primary btn-lg">
+                <i className="bi bi-plus-circle"></i> Información
+              </Link>
             </div>
           </div>
-        ))}
+        </div>
       </div>
     </>
   );
--- vite-project/src/views/Home/Home.jsx
+++ vite-project/src/views/Home/Home.jsx
@@ -1,12 +1,28 @@
 import Carousel from "../../components/Carousel/Carousel"
 import Card from "../../components/Card/Card"
 import Opinions from "../../components/Opinions/Opinions"
+import { useEffect, useState } from "react";
 
 export default function Home(){
+    const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/product");
+        const data = await response.json();
+        setProducts(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, [setProducts]);
     return(
         <>
         <Carousel/>
-        <Card/>
+        {products.map((product) => ( 
+        <Card key={product.id} nombre={product.nombre} precio={product.precio}/>))}
         <Opinions/>
         </>
     )
